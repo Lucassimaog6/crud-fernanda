1d49158e47bee8d9a490c94b9ee3079d219752dd
Lucas Ferreira Simão <22101918@aluno.cotemig.com.br>
2023-09-24 15:49:51 -0300

    Create add/delete notes functions

---
+++ notas/client/src/Components/CardNote.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let note;
+    import Trash from "/public/Trash.svg"
+
+    async function deleteNote() {
+        const response = await fetch(`http://localhost:8000/notes/${note.id}`, {
+            method: "DELETE"
+        })
+        const data = await response.json()
+        if (response.ok) {
+            console.log(data)
+        } else {
+            alert(data)
+        }
+    }
+</script>
+
+<main class="w-96 bg-yellow-200 p-4 rounded relative">
+    <p>{note.content}</p>
+    <button on:click={deleteNote} class="absolute top-2 right-2">
+        <img src={Trash} alt="Imagem do botão de excluir nota">
+    </button>
+</main>
--- notas/client/src/Pages/Home.svelte
+++ notas/client/src/Pages/Home.svelte
@@ -1,23 +1,49 @@
 <script>
-    import { navigate } from "svelte-routing";
-    import { userId } from "../Store/userId";
+	import { userId } from '../Store/userId';
+	import CardNote from '../Components/CardNote.svelte';
+    let newNoteContent = '';
 
+	let notes = [];
+	async function getNotes() {
+		const response = await fetch(`http://localhost:8000/notes/${$userId}`);
+		const data = await response.json();
+		if (response.ok) {
+			notes = data;
+			console.log(notes);
+		} else {
+			alert(data);
+		}
+	}
+	getNotes();
 
-    let notes = []
-    async function getNotes() {
-        const response = await fetch(`http://localhost:8000/notes/${$userId}`)
-        const data = await response.json()
+    async function createNote() {
+        const response = await fetch(`http://localhost:8000/notes/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userId: $userId,
+                content: newNoteContent,
+            }),
+        });
+        const data = await response.json();
         if (response.ok) {
-            notes = data
+            notes = [...notes, data];
         } else {
-            alert(data)
+            alert(data);
         }
     }
-    getNotes()
 </script>
 
-<main>
-    {#each notes as n}
-        <li>n</li>
-    {/each}
+<main class="min-h-screen grid grid-rows-[auto_1fr] gap-4 p-4 justify-center">
+	<div class="w-96 bg-yellow-200 p-4 rounded flex flex-col gap-4">
+		<button on:click={createNote} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Criar nota</button>
+		<textarea cols="40" rows="10" bind:value={newNoteContent} />
+	</div>
+	<div class="w-fit flex flex-col gap-4">
+		{#each notes as n}
+			<CardNote note={n} />
+		{/each}
+	</div>
 </main>
